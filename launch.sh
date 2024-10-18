e6f686ff0a30b55adb85c4c092b1eec4adc9ea4c
busycalibrating <19653980+busycalibrating@users.noreply.github.com>
2024-10-18 18:05:54 -0400

    update configs for mila cluster

---
+++ launch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python run_attacks.py -m ++model_name=google/gemma-2-2b-it,mistralai/Mistral-7B-Instruct-v0.3,meta-llama/Meta-Llama-3.1-8B-Instruct,qwen/Qwen2-7B-Instruct,HuggingFaceH4/zephyr-7b-beta,meta-llama/Llama-2-7b-chat-hf,ContinuousAT/Llama-2-7B-CAT,ContinuousAT/Zephyr-CAT,ContinuousAT/Phi-CAT,microsoft/Phi-3-mini-4k-instruct,cais/zephyr_7b_r2d2,GraySwanAI/Llama-3-8B-Instruct-RR,GraySwanAI/Mistral-7B-Instruct-RR ++dataset_name=adv_behaviors ++datasets.adv_behaviors.idx="range(0,300)" ++attack_name=ample_gcg ++hydra.launcher.timeout_min=120
